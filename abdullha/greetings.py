c321d4b514d8fab722662b3239a55809d91a6f96
DARK-SIRIS <xylon1590@gmail.com>
2025-06-10 05:17:30 +0600

    Create greetings.py

---
+++ abdullha/greetings.py
@@ -0,0 +1,6 @@
+def hello(name="Friend"):
+    return f"Hello, {name}! Welcome from Abdullha's package."
+
+def goodbye(name="Friend"):
+    return f"Goodbye, {name}! See you again from Abdullha's package."
+  
